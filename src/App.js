2d20bb8acc3df933dfa29429c3aa56172b687045
Tim Weiss <tdweiss1978@gmail.com>
2020-06-16 16:06:36 -0700

    Setup started

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { Component, Fragment } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Landing from './components/Landing';
-import Library from './components/Library';
-import Album from './components/Album';
 import { Navbar } from 'reactstrap';
-
-
 
 
 class App extends Component {
   render() {
     
     return (
-      <div className="App" style={{backgroundColor: "#F0EBA6" }}>
+      <div className="App">
         <h1>Welcome to RT Tile!!</h1>
       </div>
    
